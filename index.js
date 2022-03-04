06bec553b1851eac59dfdec541b043021f4bf4bb
AlejandroMolinosEligio <alejandromoeli@gmail.com>
2022-03-04 11:53:20 +0100

    Commit inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.get("/",(req,res)=>{
+    console.log("Requested / route");
+    res.send("<html><body>"+cool()+"</body></html>");
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor listo ${port}`)
+});
+
+console.log(`Servidor listo en el puerto ${port}`);
